3e1cab8e79cb887d6a4a06b616a33229596eaac6
manvendrasingh <manvendrasingh@192.168.0.6>
2018-05-01 01:18:48 +0530

    my first change

---
+++ JavaDemo/src/com/service/DumyCheck.java
@@ -0,0 +1,10 @@
+package com.service;
+
+public class DumyCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("DumyCheck.main()");
+	}
+
+}
